f11bb0ee76c7086929b213379ddff1c796b30a28
Paulius-Brickus <65765683+Paulius-Brickus@users.noreply.github.com>
2020-05-22 13:49:10 +0100

    Update myCode.py

--- pieCar/myCode.py
+++ pieCar/myCode.py
@@ -1,3 +1,21 @@
+##################################################################
+#
+# Author: Paulius Brickus
+# Date: 22/05/2020
+# 
+# Description: this code is made to controlle the car with W, A, S, and D keys.
+# References:
+#        - Evan Juras
+#        - Harrison Kinsley / Sentdex
+#        - Christopher Barnatt / ExplainingComputer.com
+#        - Alex Krizhevsky, Ilya Sutskever & Geoffrey E. Hinton. ImageNet
+#          Classification with Deep Convolutional Neural Networks. NIPS, 2012.
+# Links:
+#     - https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi
+#     - https://github.com/sentdex/pygta5/
+#     - https://www.explainingcomputers.com/rasp_pi_robotics.html
+#     - [AlexNet Paper](http://papers.nips.cc/paper/4824-imagenet-classification-with-deep-convolutional-neural-networks.pdf)
+
 
 
 #Import classes, functions, variables, from "mdev" files
@@ -9,8 +27,7 @@ from mDev import *
 mdev = mDEV()#create object
 
 
-# Get the curses window, turn off echoing of keyboard to screen, turn on
-# instant (no waiting) key response, and use special values for cursor keys
+# Get the curses window,disable echoing of keyboard to the while code is runining screen,
 screen = curses.initscr()
 curses.noecho() 
 
